6e21c23d83f604046462da3b23b6bb0db0f74197
laubenitez79 <lauti.benitez.nahuel@gmail.com>
2023-05-28 03:20:07 -0300

    Actualizamos codigo para mejor funcionamiento

---
+++ index.js
@@ -0,0 +1,48 @@
+require('dotenv').config()
+
+const http = require("http")
+const fs = require("fs")
+
+function requestController(req , res) {
+    const url = req.url 
+    const method = req.method
+    console.log({url , method});
+
+    if (method === "GET" && url === "/") {
+        res.setHeader("Content-type" , "text/html; charset=utf-8")
+        fs.readFile("./public/index.html", function (err, file) {
+            if (err) {
+                console.log("Hubo un error");
+            } 
+            res.write(file)
+            res.end()
+            
+        })
+        return
+    }
+    if (method === "GET" && url === "/about") {
+        res.setHeader("Content-type" , "text/html; charset=utf-8")
+        fs.readFile("./public/about.html", function (err, file) {
+            if (err) {
+                console.log("Hubo un error");
+            } 
+            res.write(file)
+            res.end()
+            
+        })
+        return
+    }
+
+    res.setHeader("Content-type" , "text/html; charset=utf-8")
+    res.write("<h1>Pagina no encontrada</h1>")
+    res.end()
+    return
+}
+
+const PORT = process.env.PORT;
+
+const server = http.createServer(requestController)
+
+server.listen(PORT , function () {
+    console.log("Aplicacion corriendo en el puerto " + PORT);
+})
