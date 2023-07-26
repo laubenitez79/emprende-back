e29c28e06ea3e60fd685125d7fdde8f4b31f3698
laubenitez79 <laubenitez@uade.edu.ar>
2023-07-26 20:43:27 -0300

    Agregamos cosas al front

--- public/index.js
+++ public/index.js
@@ -1,1 +1,9 @@
-console.log("Hola")
+console.log("Va a ser interpretado por el navegador")
+
+const button = document.querySelector('button')
+console.log({button})
+
+button.addEventListener("click" , function () {
+    console.log("Presionaste el boton")
+    fetch("https://emprende-back-2.onrender.com/users")
+})
