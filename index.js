9be8baa11d4b2f762b8edc84a891389b29b68b64
laubenitez79 <laubenitez@uade.edu.ar>
2023-07-26 02:37:38 -0300

    nuevos cambios

--- index.js
+++ index.js
@@ -1,52 +1,56 @@
-const http = require("http");
-const fs = require("fs");
+require('dotenv').config()
 
-function requestController(req, res) {
-    const url = req.url;
-    const method = req.method;
-    console.log({ url, method });
+const http = require("http")
+const fs = require("fs")
+
+function requestController(req , res) {
+    const url = req.url 
+    const method = req.method
+    console.log({url , method});
 
     if (method === "GET" && url === "/") {
-        res.setHeader("Content-type", "text/html; charset=utf-8");
+        res.setHeader("Content-type" , "text/html; charset=utf-8")
         fs.readFile("./public/index.html", function (err, file) {
-        if (err) {
-            console.log("Hubo un error al leer el archivo:", err);
-            res.statusCode = 500;
-            res.write("<h1>Error interno del servidor</h1>");
-            res.end();
-            return;
-        }
-        res.write(file);
-        res.end();
-        });
-        return;
+            if (err) {
+                console.log("Hubo un error al leer el archivo:", err);
+                res.statusCode = 500;
+                res.write("<h1>Error interno del servidor</h1>");
+                res.end();
+                return;
+            } 
+            res.write(file)
+            res.end()
+            
+        })
+        return
     }
-
     if (method === "GET" && url === "/about") {
-        res.setHeader("Content-type", "text/html; charset=utf-8");
+        res.setHeader("Content-type" , "text/html; charset=utf-8")
         fs.readFile("./public/about.html", function (err, file) {
-        if (err) {
-            console.log("Hubo un error al leer el archivo:", err);
-            res.statusCode = 500;
-            res.write("<h1>Error interno del servidor</h1>");
-            res.end();
-            return;
-        }
-        res.write(file);
-        res.end();
-        });
-        return;
+            if (err) {
+                console.log("Hubo un error al leer el archivo:", err);
+                res.statusCode = 500;
+                res.write("<h1>Error interno del servidor</h1>");
+                res.end();
+                return;
+            } 
+            res.write(file)
+            res.end()
+            
+        })
+        return
     }
 
-    res.setHeader("Content-type", "text/html; charset=utf-8");
-    res.write("<h1>Pagina no encontrada</h1>");
-    res.end();
+    res.setHeader("Content-type" , "text/html; charset=utf-8")
+    res.write("<h1>Pagina no encontrada</h1>")
+    res.end()
+    return
 }
 
-const PORT = process.env.PORT 
+const PORT = process.env.PORT;
 
-const server = http.createServer(requestController);
+const server = http.createServer(requestController)
 
-server.listen(PORT, function () {
+server.listen(PORT , function () {
     console.log("Aplicacion corriendo en el puerto " + PORT);
-});
+})
