9bed3637f25d10be9e12b2bd179e344bda533ffa
laubenitez79 <laubenitez@uade.edu.ar>
2023-08-09 13:13:13 -0300

    Cambios

--- index.js
+++ index.js
@@ -2,14 +2,88 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const port = process.env.PORT
+const mongoose = require("mongoose")
+const Schema = mongoose.Schema
+
+mongoose.connect(process.env.MONGODB_URL).then(() => {
+    console.log("Conexion exitosa a la BBDD")
+}).catch((err) => {
+    console.log("Hubo un error al conectarnos a la BBDD" , {err})
+})
+
+const taskSchema = new Schema({
+    name : String,
+    done : Boolean,
+    // createdBy 
+})
+
+const Task = mongoose.model("Task" , taskSchema, "Tasks")
 
 app.use(express.static('public'))
+
+app.use(express.json())
+
+app.use((req, res , next) => {
+    next()
+})
 
 app.get('/' , (req, res)=>{
     res.send('Hello world')
 })
+
 app.get('/users' , (req, res)=>{
     res.send([{name: "Lautaro"}, {name: "Pedro"}])
+})
+
+app.get('/api/tasks' , (req, res)=>{
+    Task.find().then((tasks) => {
+        res.status(200).json({ok : true, data : tasks})
+    }).catch((err) => {
+        res.status(400).json({ok: false , message : "Hubo un problema al encontrar una tarea"})
+    })
+    
+})
+
+app.post('/api/tasks' , (req, res)=>{
+    const body = req.body
+    Task.create({
+        name : body.text,
+        done : false,
+        hello: "HOLA",
+    }).then((createdTask) => {
+        res.status(201)
+        .json({
+            ok: true,
+            message: "Tarea creada con exito",
+            data: createdTask
+        })
+    }).catch((err) => {
+        res.status(400).json({ok : false, message : "Error al crear la tarea"})
+    })
+    
+})
+
+app.delete('/api/tasks/:id' , (req, res)=>{
+    const id = req.params.id
+    Task.findByIdAndRemove(id).then((deleteTask) => {
+        res.status(201).json({ ok: true, message: "Tarea borrada con exito", data: deleteTask})
+    }).catch((err) => {
+        res.status(400).json({ok : false, message : "Error al borrar la tarea"})
+    })
+    
+})
+
+app.put('/api/tasks/:id' , (req, res)=>{
+    const body = req.body
+    const id = req.params.id
+    Task.findByIdAndUpdate(id, {
+        name : body.text,
+    }).then((updatedTask) => {
+        res.status(200).json({ ok: true, message: "Tarea editada con exito", data: updatedTask})
+    }).catch((err) => {
+        res.status(400).json({ok : false, message : "Error al editar la tarea"})
+    })
+    
 })
 
 app.listen(port ,() => {
--- public/index.js
+++ public/index.js
@@ -1,9 +1,65 @@
-console.log("Va a ser interpretado por el navegador")
+const createEditButton = document.querySelector('#postButton')
+const inputTask = document.querySelector('#task-name')
+const tasksContainer = document.querySelector("#tasks-container")
+let TASK_TO_EDIT = null
 
-const button = document.querySelector('button')
-console.log({button})
+const baseBackEndUrl = "http://localhost:4000/api"
 
-button.addEventListener("click" , function () {
-    console.log("Presionaste el boton")
-    fetch("https://emprende-back-2.onrender.com/users")
+createEditButton.addEventListener("click" , function () {
+    const creating = !TASK_TO_EDIT
+    const path = creating ? "tasks" : `tasks/${TASK_TO_EDIT._id}`
+    const method = creating ? "POST" : "PUT"
+    fetch(`${baseBackEndUrl}/${path}` , {
+        method,
+        headers : { "Content-Type" : "application/json" },
+        body : JSON.stringify({text: inputTask.value})
+    })
+    .then((res) => {
+        getTasks()
+        inputTask.value = ""
+        createEditButton.innerText = "Crear tarea"
+        return res.json()
+    })
+    .then((resJSON) => {
+        console.log({resJSON})
+    })
 })
+
+function getTasks() {
+    tasksContainer.innerHTML = null
+    fetch(`${baseBackEndUrl}/tasks`)
+        .then((res) => res.json())
+        .then((resJSON) => {
+            const tasks = resJSON.data
+            for (const task of tasks) {
+                const containerTask = document.createElement('div')
+                const taskParagraph = document.createElement('p')
+                const deleteTaskBtn = document.createElement('button')
+                containerTask.setAttribute('class' , 'container-task')
+                deleteTaskBtn.setAttribute('id' , `${task._id}`)
+                taskParagraph.innerText = task.name
+                deleteTaskBtn.innerText = "Delete"
+                deleteTaskBtn.addEventListener('click' , (e) => {
+                    const taskID = e.target.id
+                    deleteTaskBtn.innerText = "..."
+                    fetch(`${baseBackEndUrl}/tasks/${taskID}`, {
+                        method: "DELETE",
+                    }).then(() => {
+                        const taskDiv = deleteTaskBtn.parentElement
+                        taskDiv.remove()
+                    })
+                })
+                taskParagraph.addEventListener('click' , (e) => {
+                    inputTask.value = task.name
+                    createEditButton.innerText = "Editar tarea"
+                    TASK_TO_EDIT = task
+                    console.log({TASK_TO_EDIT})
+                })
+                containerTask.appendChild(taskParagraph)
+                containerTask.appendChild(deleteTaskBtn)
+                tasksContainer.appendChild(containerTask)
+            }
+    })
+}
+
+getTasks()
